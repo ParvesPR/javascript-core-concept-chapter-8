572603faef6c3ff3f7a42c5594f0f03da5db3c19
ParvesPR <parves2585@gmail.com>
2024-04-26 15:05:53 +0800

    functional programming intro

---
+++ intro.js
@@ -0,0 +1,9 @@
+// Functional Programming
+/* 
+Functional programming (FP) is a programming paradigm that uses pure functions to build computer programs without mutating data or state.
+
+Main three term of Functional Programming
+1. Pure Function
+1. First Class Function
+1. Higher Order Function
+ */
