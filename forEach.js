144e21052f4b74148a9492539163fa789480da49
ParvesPR <parves2585@gmail.com>
2024-05-14 13:43:03 +0800

    for each behind the scene

---
+++ forEach.js
@@ -0,0 +1,21 @@
+var arr = [1, 2, 3, 4, 5];
+var sum = 0;
+// TRAVERSE AN ARRAY USING FOREACH  FUNCTION
+// arr.forEach(function (value, index, arr) {
+//     console.log(value, index, arr);
+//     sum += value;
+// })
+// console.log(sum);
+
+// BEHIND THE SCENE FOREACH FUNCTION
+function forEach(arr, cb) {
+    for (var x = 0; x < arr.length; x++) {
+        cb(arr[x], x, arr)
+    }
+}
+var sum = 0;
+forEach(arr, function (value, index, arr) {
+    console.log(value, index, arr);
+    sum += value;
+})
+console.log(sum);
