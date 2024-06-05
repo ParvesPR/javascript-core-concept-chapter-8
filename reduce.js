a3c4bcd577de89a66df144961ed54429f17ce3e7
ParvesPR <parves2585@gmail.com>
2024-06-05 13:24:00 +0800

    filter max value using reduce methos

--- reduce.js
+++ reduce.js
@@ -1,5 +1,10 @@
-var arr = [1, 2, 3, 4, 5];
+var arr = [1, 2, 13, 4, 5];
 var sum = arr.reduce(function (prev, curr, value, index) {
     return prev + curr;
-})
+}, 10)
 console.log(sum);
+
+var max = arr.reduce(function (prev, curr) {
+    return Math.max(prev, curr)
+})
+console.log(max);
