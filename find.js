5df13a3b59376b1817397a0d1ab5212e4a24a69e
ParvesPR <parves2585@gmail.com>
2024-06-11 15:19:56 +0800

    array find method implementation

--- find.js
+++ find.js
@@ -1,10 +1,23 @@
 var arr = [1, 5, 7, 9, 3, 6, 14, 92];
-var result = arr.find(function (value, index) {
+// var result = arr.find(function (value, index) {
+//     return value === 9;
+// })
+// console.log(result);
+
+// var result2=arr.findIndex(function (value,index) {
+//     return value===9;
+// })
+// console.log(result2);
+
+// Array find method implementation
+function myFind(arr, cb) {
+    for (var x = 0; x < arr.length; x++) {
+        if (cb(arr[x])) {
+            return arr[x];
+        }
+    }
+}
+var findArr = myFind(arr, function (value) {
     return value === 9;
 })
-console.log(result);
-
-var result2=arr.findIndex(function (value,index) {
-    return value===9;
-})
-console.log(result2);
+console.log(findArr);
